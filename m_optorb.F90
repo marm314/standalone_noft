3477298de2e1933c0ba0c0dfc79d15703ef8588c
M. Rodriguez-Mayorga <marm3.14@gmail.com>
2022-09-30 10:34:19 +0200

    Include trhshold for jumping

--- m_optorb.F90
+++ m_optorb.F90
@@ -204,7 +204,7 @@ subroutine opt_orb(iter,imethod,ELAGd,RDMd,INTEGd,Vnn,Energy,mo_ints,NO_COEF,NO_
 !-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --       
  enddo
  
- if(ELAGd%real_grad.and.iter/=0) then
+ if(ELAGd%real_grad.and.iter/=0.and.Ediff<ten**-FOUR) then
   ELAGd%real_grad=.false.
  else
   ELAGd%real_grad=.true.
